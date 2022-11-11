4dcb632e83906b06bce41171fd0427af5aec7012
PatrickDuong3001 <baophuc2000@gmail.com>
2022-11-11 01:20:20 -0500

    first enemy is able to chase the player

--- mainGame.py
+++ mainGame.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 import pickle
 import threading
-import time
+import math
 
 class character(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -93,7 +93,6 @@ class character(pygame.sprite.Sprite):
     
     def draw_character(self):
         screen.blit(pygame.transform.flip(self.image, self.flip_character, False), self.rect)
-        
 
 class FireJutsu(pygame.sprite.Sprite):
     def __init__(self, x, y, direction):
@@ -193,7 +192,68 @@ class setCheck:
             print("Hello")
             return 1
         return -1
+
+class BasicEnemy(pygame.sprite.Sprite): 
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.alive = True
+        self.health = 100
+        self.max_health = self.health
+        self.update_time = pygame.time.get_ticks()
+        self.action_type = 0
+        self.flip_character = False
+        self.f_ind = 0    
+        self.animation_list = []    
+        animation_types = ["basicEnemyRun"]
         
+        for animation in animation_types:
+            temp = []
+            for i in range(len(os.listdir(f'animation/{animation}'))):
+                img = pygame.image.load(f'animation/{animation}/{i}.png').convert_alpha()
+                img = pygame.transform.scale(img, (int(1.2*img.get_width()), int(1.2*img.get_height())))
+                temp.append(img)
+            self.animation_list.append(temp)
+        self.image = self.animation_list[self.action_type][self.f_ind]
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+    
+    def animate_updater(self):
+        #based on current frame, updating image
+        self.image = self.animation_list[self.action_type][self.f_ind]
+        if pygame.time.get_ticks() - self.update_time > 100:
+            self.update_time = pygame.time.get_ticks()
+            self.f_ind += 1
+        if self.f_ind >= len(self.animation_list[self.action_type]):
+            if self.action_type == 3:
+                self.f_ind = len(self.animation_list[self.action_type]) - 1
+            else:
+                self.f_ind = 0
+    
+    def action_updater(self, action):
+        #check if the new action is different to the previous one
+        if action != self.action_type:
+            self.action_type = action
+            #update the animation settings
+            self.f_ind = 0
+            self.update_time = pygame.time.get_ticks()    
+    
+    def move_towards_player(self, player):
+        # Find direction vector (dx, dy) between enemy and player.
+        dx, dy = player.rect.x - self.rect.x, player.rect.y - self.rect.y
+        
+        if player.rect.x - self.rect.x < 0:
+            self.flip_character = True
+        else: 
+            self.flip_character = False
+        
+        dist = math.hypot(dx, dy)
+        dx, dy = dx / dist, dy / dist  # Normalize.
+        # Move along this normalized vector towards the player at current speed.
+        self.rect.x += dx * basicEnemySpeed
+        self.rect.y += dy * basicEnemySpeed
+    
+    def draw_character(self):
+        screen.blit(pygame.transform.flip(self.image, self.flip_character, False), self.rect)
         
 ##############################################################Main Game########################################################################
 handsigns = [] #set of handsigns
@@ -215,11 +275,13 @@ fire_flip = False
 fire_shoot_stance = False
 fire_shoot_stance_dur = 0
 jutsu_perform = False
+basicEnemySpeed = 3
 
 fire_ball = pygame.image.load('fire.png').convert_alpha()
 fire_ball_flip = pygame.transform.flip(fire_ball,True,False)
 fire_sprite_group = pygame.sprite.Group()
 sasuke = character(50, 200)
+basicEnemy_1 = BasicEnemy(500,200)
 
 BG = (144, 201, 120)
 RED = (255, 0, 0)
@@ -244,6 +306,10 @@ while run:
     #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$player and enemies control$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
     sasuke.update()
     sasuke.draw_character()
+    
+    basicEnemy_1.animate_updater()
+    basicEnemy_1.move_towards_player(sasuke)
+    basicEnemy_1.draw_character()
 
     fire_sprite_group.update()
     fire_sprite_group.draw(screen)
